440bc75ecc21c33c814aa942718b42087b549021
Nikita Dagar <nikita@digitalgreen.org>
2013-09-30 18:58:43 +0530

    +Add collection

---
+++ dg/media/social_website/scripts/app/view-controllers/CollectionAddController.js
@@ -0,0 +1,100 @@
+/**
+ * VideoAddController Class File
+ *
+ * @author aadish
+ * @version $Id$
+ * @requires require.js
+ * @requires jQuery
+ */
+
+define(function(require) {
+    'use strict';
+    var DigitalGreenPageController = require('app/controllers/DigitalGreenPageController');
+    var Controller = require('framework/controllers/Controller');
+    var viewRenderer = require('framework/ViewRenderer');
+    var jQuery = require('jquery');
+    var videoFormTemplate = require('text!app/views/video-add-form.html');
+    var Chosen = require('libs/external/chosen.jquery.min');
+    
+    var CollectionAddController = Controller.extend({
+
+        /**
+         * Controller constructor
+         * @return {Controller} this
+         */
+        constructor: function($referenceBase) {
+            this.base(bootstrapConfig, globalHelpers);
+            this.base($referenceBase);
+            this._renderVideoFormItems();
+            this._dropdownChosen();
+            return this;
+        },
+
+        _initReferences: function($referenceBase) {
+            this.base();
+            var references = this._references;
+          //references.dataFeed = new FeaturedCollectionDataFeed($language);
+            references.$videoAddWrapper = $referenceBase;
+            references.$videoAddContainer = $referenceBase.find('.js-video-add-container');
+            references.$videoAddMoreButton = $referenceBase.find('.js-add-more-videos-btn');
+        },
+
+        _initEvents: function() {
+            this.base();
+            
+            var boundFunctions = this._boundFunctions;
+            var references = this._references;
+            
+            /*boundFunctions.onDataProcessed = this._onDataProcessed.bind(this);
+            references.dataFeed.on('dataProcessed', boundFunctions.onDataProcessed);*/
+            
+            /*// input param changed alert from data feed
+            boundFunctions.onInputParamChanged = this._onInputParamChanged.bind(this);
+            references.dataFeed.on('inputParamChanged', boundFunctions.onInputParamChanged)*/
+            
+            //adding another video form
+            boundFunctions.onAddMoreVideoFormClick = this._onAddMoreVideoFormClick.bind(this);
+            references.$videoAddMoreButton.on("click", boundFunctions.onAddMoreVideoFormClick);
+        },
+
+        _dropdownChosen: function(){
+            $(".chosen-select").chosen({no_results_text: "No results match", width: "90%"});
+        },
+
+        _onDataProcessed: function() {
+            
+        },
+
+        _renderVideoFormItems: function() {
+
+            viewRenderer.renderAppend(this._references.$videoAddContainer, videoFormTemplate);
+
+        },
+
+        _onAddMoreVideoFormClick: function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            this._renderVideoFormItems();
+            this._dropdownChosen();
+        },
+        
+        setInputParam: function(key, value, disableCacheClearing) {
+            this._references.dataFeed.setInputParam(key, value, disableCacheClearing);
+        },
+
+        _onInputParamChanged: function() {
+            this.getFeaturedCollection();
+        },
+        /**
+         * Controller destructor
+         * @return {void}
+         */
+        destroy: function() {
+            this.base();
+
+            // TODO: clean up
+        }
+    });
+
+    return CollectionAddController;
+});
