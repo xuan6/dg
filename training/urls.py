3b4f22e152ac6e62ebb3aa4908b2ac216f4ac895
Tanmay Goel <goeltanmay@gmail.com>
2015-09-17 16:14:50 +0530

    bugfixes

--- training/urls.py
+++ training/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 from tastypie.api import Api
-from training.api import TrainerResource, LanguageResource, AssessmentResource, QuestionResource, TranslationResource, MediatorResource
+from training.api import TrainerResource, LanguageResource, AssessmentResource, QuestionResource, MediatorResource, DistrictResource, VillageResource, PartnerResource
 from views import login
 
 api = Api(api_name = "v1")
@@ -11,9 +11,10 @@ api.register(TrainerResource())
 api.register(LanguageResource())
 api.register(AssessmentResource())
 api.register(QuestionResource())
-api.register(TranslationResource())
 api.register(MediatorResource())
-
+api.register(DistrictResource())
+api.register(VillageResource())
+api.register(PartnerResource())
 
 urlpatterns = patterns('',
 	(r'^login/', login),
