a461801f4931d173bbf7ece20581b701a0b89aa7
Tanmay Goel <goeltanmay@gmail.com>
2015-10-29 14:53:39 +0530

    bug fix. script to edit adoptions.

---
+++ farmerbook/management/commands/adoption_add_script_usaid.py
@@ -0,0 +1,6 @@
+from activities.models import PersonAdoptPractice, PersonMeetingAttendance
+list_ids = [(67200,69),(86698,64),(86662,72),(65530,58),(98333,75),(75619,68),(62567,61),(72560,67),(72495,63)]
+for tup in list_ids:
+	att = PersonMeetingAttendance.objects.filter(person__id = tup[0])
+	length = len(att)
+	print length
