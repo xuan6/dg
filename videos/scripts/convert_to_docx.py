198c180106155193cccac9709023a4a8b0d0da3f
Jahnavi Kalyani <jahnavi@digitalgreen.org>
2016-01-08 12:29:41 +0530

    updated script to write to file

--- videos/scripts/convert_to_docx.py
+++ videos/scripts/convert_to_docx.py
@@ -5,6 +5,7 @@ from docx.shared import Inches
 
 from django.db.models import get_model
 from dg.settings import MEDIA_ROOT
+from dg.base_settings import STATIC_URL
 
 from videos.scripts.exception_email import sendmail
 
@@ -15,11 +16,74 @@ def save_as_document(video_id):
 		Video = get_model('videos', 'Video')
 		video_obj = Video.objects.get(id=video_id)
 
+		practice_name = ''
+		benefits = ''
+
+		if video_obj.related_practice:
+			if video_obj.related_practice.practice_name:
+				practice_name=practice_name+str(video_obj.related_practice.practice_name)+', '
+			if video_obj.related_practice.practice_sector:
+				practice_name=practice_name+str(video_obj.related_practice.practice_sector)+', '
+			if video_obj.related_practice.practice_subsector:
+				practice_name=practice_name+str(video_obj.related_practice.practice_subsector)+', '
+			if video_obj.related_practice.practice_topic:
+				practice_name=practice_name+str(video_obj.related_practice.practice_topic)+', '
+			if video_obj.related_practice.practice_subtopic:
+				practice_name=practice_name+str(video_obj.related_practice.practice_subtopic)+', '
+			if video_obj.related_practice.practice_subject:
+				practice_name=practice_name+str(video_obj.related_practice.practice_subject)
+
+		if video_obj.summary:
+			benefits = benefits+str(video_obj.summary)
+
+		logo = os.path.join("dg", "media", "output", "images", "dglogo.png")
+
 		document = Document()
-		document.add_heading(video_obj.title, 0)
+		# document.add_heading(video_obj.title, 0)
+		# for obj in nonnegotiables_list:
+		# 	text = obj.non_negotiable
+		# 	document.add_paragraph(text)
+
+		document.add_picture(logo, width=Inches(1.10))
+
+		table_1 = document.add_table(rows=1, cols=1)
+		table_1.style = 'TableGrid'
+
+		hdr_cells_1 = table_1.rows[0].cells
+		hdr_cells_1[0].text = 'Video Non-Negotiables Guide'
+		hdr_cells_1[0].alignment = 1
+		#hdr_cells_1[0].style = 'Center'
+
+		row_cells = table_1.add_row().cells
+		row_cells[0].text = 'Video Name: '+video_obj.title
+		row_cells = table_1.add_row().cells
+		row_cells[0].text = 'Video Category: '+practice_name
+		row_cells = table_1.add_row().cells
+		row_cells[0].text = 'Benefits: '+benefits
+		row_cells = table_1.add_row().cells
+		row_cells[0].text = 'Video:     [  ] For adoption verification    [  ] Only for understanding'
+
+		
+		table = document.add_table(rows=1, cols=2)
+		table.style = 'ColorfulList-Accent3'
+
+		hdr_cells = table.rows[0].cells
+		hdr_cells[1].text = 'Physically Verifiable'
+		
+		count = 1
 		for obj in nonnegotiables_list:
-			text = obj.non_negotiable
-			document.add_paragraph(text)
+			nonnegotiable = obj.non_negotiable
+			row_cells = table.add_row().cells
+			row_cells[0].text = 'NON-NEGOTIABLE '+str(count)+': '+str(nonnegotiable)
+			row_cells = table.add_row().cells
+			row_cells[0].text = '\n'
+			count = count + 1
+
+
+
+		#table.rows[0].style = "borderColor:red; background-color:gray"
+
+
 		
 		#===================================================================
 		# Formatting options:
@@ -34,6 +98,7 @@ def save_as_document(video_id):
 		# document.add_paragraph(
 		# 	'first item in unordered list', style='ListBullet'
 		# )
+		# document.add_picture('monty-truth.png', width=Inches(1.25))
 		# document.add_paragraph(
 		# 	'first item in ordered list', style='ListNumber'
 		# )
@@ -46,4 +111,5 @@ def save_as_document(video_id):
 
 	except Exception as e:
 		error = "Video ID: "+str(video_id)+" Error: "+str(e)
-		sendmail("Exception in creating document for Non Negotiables", error)
+		#sendmail("Exception in creating document for Non Negotiables", error)
+		print error
